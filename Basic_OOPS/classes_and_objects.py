2f8be0f5d199ce114a1940664b92b1246fc62968
Divya <divvis888@gmail.com>
2025-01-09 18:59:03 -0800

    reformat

--- Basic_OOPS/classes_and_objects.py
+++ Basic_OOPS/classes_and_objects.py
@@ -14,8 +14,7 @@ class Interviews:
 
     # @method
     def interviewProcess(self):
-        print(f"I have an Interview at {self.companyName} which is a {
-              self.interviewLevel} Interview")
+        print(f"I have an Interview at {self.companyName} which is a {self.interviewLevel} Interview")
 
 
 interview_one = Interviews('AWS', 'Phone Screen')
