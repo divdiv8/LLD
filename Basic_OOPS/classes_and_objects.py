17a3e1932742a018b101cf1d0dbf8996e1c44209
Pratik Thorat <thorat.p@northeastern.edu>
2025-01-09 21:57:22 -0500

    Re organization

---
+++ Basic_OOPS/classes_and_objects.py
@@ -0,0 +1,25 @@
+'''
+Classes and Objects Example
+'''
+
+# Class declaration
+# _ as a prefix determines private fields and constructors.
+
+
+class Interviews:
+    # @constructor
+    def __init__(self, companyName, interviewLevel):
+        self.companyName = companyName
+        self.interviewLevel = interviewLevel
+
+    # @method
+    def interviewProcess(self):
+        print(f"I have an Interview at {self.companyName} which is a {
+              self.interviewLevel} Interview")
+
+
+interview_one = Interviews('AWS', 'Phone Screen')
+interview_one.interviewProcess()
+
+interview_two = Interviews('ByteDance', 'Online Assessment')
+interview_two.interviewProcess()
